45469060c1c43156304ce03ae36a5180f1375ba2
KaletJosue <kalet.jo28@gmail.com>
2025-01-07 17:07:32 -0500

    first commit

---
+++ controller/start/start.js
@@ -0,0 +1,77 @@
+var loader = document.querySelector('.loader')
+var logo = document.querySelector('.nav img')
+var li = document.querySelectorAll('.nav ul li')
+var textHero = document.querySelector('.textHero')
+var ham = document.querySelector('.hero img')
+var down = document.querySelector('#down')
+
+window.addEventListener('load', function() {
+    setTimeout(function() {
+
+        loader.classList.add('active')
+        logo.classList.add('active')
+        li.forEach((option) => {
+            option.classList.add('active')
+        })
+        textHero.classList.add('active')
+        ham.classList.add('active')
+        down.classList.add('active')
+
+    }, 2000);
+});
+
+var video = document.querySelector('#video')
+const playPauseBtn = document.getElementById('playPauseBtn');
+
+playPauseBtn.addEventListener('click', () => {
+    document.addEventListener('keydown', function (event) {
+        if (event.code === 'Space') {
+            if (video.paused) {
+                video.play();
+                playPauseBtn.classList.add('active')
+            } else {
+                video.pause();
+                playPauseBtn.classList.remove('active')
+            }
+        }
+    });
+    if (video.paused) {
+        video.play();
+        playPauseBtn.classList.add('active')
+    } else {
+        video.pause();
+        playPauseBtn.classList.remove('active')
+    }
+});
+
+video.addEventListener('click', () => {
+    document.addEventListener('keydown', function (event) {
+        if (event.code === 'Space') {
+            if (video.paused) {
+                video.play();
+                playPauseBtn.classList.add('active')
+            } else {
+                video.pause();
+                playPauseBtn.classList.remove('active')
+            }
+        }
+    });
+    if (video.paused) {
+        video.play();
+        playPauseBtn.classList.add('active')
+    } else {
+        video.pause();
+        playPauseBtn.classList.remove('active')
+    }
+});
+
+video.addEventListener('ended', () => {
+    playPauseBtn.classList.remove('active')
+    video.currentTime = 0;
+});
+
+var contac = document.querySelector('.contac')
+
+contac.addEventListener('click', () => {
+    window.open('https://wa.me/573229645600', '_blanck')
+})
