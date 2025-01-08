6f8a7a1689a63ef07f5fa826280343fbeb0359af
KaletJosue <kalet.jo28@gmail.com>
2025-01-08 11:48:02 -0500

    menu responsive

--- controller/start/start.js
+++ controller/start/start.js
@@ -1,24 +1,35 @@
-var loader = document.querySelector('.loader')
-var logo = document.querySelector('.nav img')
-var li = document.querySelectorAll('.nav ul li')
-var textHero = document.querySelector('.textHero')
-var ham = document.querySelector('.hero img')
-var down = document.querySelector('#down')
+var openMenu = document.getElementById('openMenu')
+var closeMenu = document.getElementById('closeMenu')
+var menu = document.querySelector('.nav ul')
+var optionsMenu = document.querySelectorAll('.nav ul li')
 
-window.addEventListener('load', function() {
-    setTimeout(function() {
+window.addEventListener('click', event => {
+    if (event.target == menu) {
+        closeMenu.classList.remove('active')
+        openMenu.classList.remove('active')
+        menu.classList.remove('active')
+    }
+})
 
-        loader.classList.add('active')
-        logo.classList.add('active')
-        li.forEach((option) => {
-            option.classList.add('active')
-        })
-        textHero.classList.add('active')
-        ham.classList.add('active')
-        down.classList.add('active')
+optionsMenu.forEach((optionMenu) => {
+    optionMenu.addEventListener('click', () => {
+        closeMenu.classList.remove('active')
+        openMenu.classList.remove('active')
+        menu.classList.remove('active')
+    })
+})
 
-    }, 2000);
-});
+openMenu.addEventListener('click', () => {
+    closeMenu.classList.add('active')
+    openMenu.classList.add('active')
+    menu.classList.add('active')
+})
+
+closeMenu.addEventListener('click', () => {
+    closeMenu.classList.remove('active')
+    openMenu.classList.remove('active')
+    menu.classList.remove('active')
+})
 
 var video = document.querySelector('#video')
 const playPauseBtn = document.getElementById('playPauseBtn');
